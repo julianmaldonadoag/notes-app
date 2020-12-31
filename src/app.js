9e585be1e763e455eadffa378c63920c6a5ce326
Angel Julian Maldonado Aguilar <angelmaldonadoag@gmail.com>
2020-12-30 22:53:35 -0500

    Basic web server

---
+++ src/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Settings.
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+
+// Middlewares.
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+// Routes.
+app.get('/', (req, res) => {
+  res.send('En contrucción');
+})
+
+// Static files.
+app.use(express.static(path.join(__dirname, 'public')));
+
+module.exports = app;
