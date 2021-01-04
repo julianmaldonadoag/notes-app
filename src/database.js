f11f0cf34aefd2af828e9db86edc42ad36cff8aa
Angel Julian Maldonado Aguilar <angelmaldonadoag@gmail.com>
2021-01-03 19:23:22 -0500

    Connecting DB using mongoose
    Added .env file and some env vars

---
+++ src/database.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+const { NOTES_APP_MONGODB_HOST, NOTES_APP_MONGODB_DB } = process.env
+
+const MONGODB_URI = `mongodb://${NOTES_APP_MONGODB_HOST}/${NOTES_APP_MONGODB_DB}`
+
+mongoose.connect(MONGODB_URI, { 
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+})
+  .then(db => console.log('Database is connected'))
+  .catch(err => console.log(err))
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+require('./database')
 const app = require('./app');
 
 app.listen(app.get('port'), () => {
