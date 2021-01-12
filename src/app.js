14659b9b0038424ea176363b912f5f0074d037a8
Angel Julian Maldonado Aguilar <angelmaldonadoag@gmail.com>
2021-01-11 22:33:48 -0500

    First routes with express

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const app = express();
 
 // Settings.
 app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views'))
 app.engine('.hbs', exphbs({
   defaultLayout: 'main',
   layoutsDir: path.join(app.get('views'), 'layouts'),
@@ -17,14 +17,12 @@ app.set('view engine', '.hbs')
 
 // Middlewares.
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({extended: false}))
 
 // Routes.
-app.get('/', (req, res) => {
-  res.render('index')
-})
+app.use(require('./routes/index.routes'))
 
 // Static files.
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')))
 
 module.exports = app;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 require('./database')
-const app = require('./app');
+const app = require('./app')
 
 app.listen(app.get('port'), () => {
   console.log(`Server running on port ${app.get('port')}`);
