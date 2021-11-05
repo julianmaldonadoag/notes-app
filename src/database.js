4d0cc4600d632127566de06949aefdfa1d11b581
Angel Julian Maldonado Aguilar <julianmaldonadoag@gmail.com>
2021-11-05 14:39:59 -0500

    Update MongoDB uri, select the local or production env var

--- src/database.js
+++ src/database.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose')
-const { NOTES_APP_MONGODB_HOST, NOTES_APP_MONGODB_DB } = process.env
+const { MONGODB_URI, NOTES_APP_MONGODB_HOST, NOTES_APP_MONGODB_DB } = process.env
 
-const MONGODB_URI = `mongodb://${NOTES_APP_MONGODB_HOST}/${NOTES_APP_MONGODB_DB}`
+const DB_URI = MONGODB_URI || `mongodb://${NOTES_APP_MONGODB_HOST}/${NOTES_APP_MONGODB_DB}`
 
-mongoose.connect(MONGODB_URI, { 
+mongoose.connect(DB_URI, { 
   useUnifiedTopology: true,
   useNewUrlParser: true,
   useFindAndModify: false
